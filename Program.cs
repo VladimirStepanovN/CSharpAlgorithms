679aa8faa83c019ad49f09281c1db3951c065cec
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-20 00:14:12 +0500

    Добавлена структура данных числовой массив и линейный поиск.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace CSharpAlgorithms
+﻿using CSharpAlgorithms.Structure;
+
+namespace CSharpAlgorithms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //----------------------------структура массив чисел-----------------------------------
+            DigitsArray da = new DigitsArray(5);
+            da.Insert(1);
+            da.Insert(2);
+            da.Show(); //выводит: 1 2
+            if(da.Find(1)) da.Delete(1);
+            da.Show(); //выводит: 2
+            da.Insert(3);
+            da.Insert(4);
+            da.Show(); //выводит 2 3 4
+            //-------------------------------------------------------------------------------------
         }
     }
 }
