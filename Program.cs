458d0901a37029cb6e36c930dcd14e7602f0eb49
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-20 00:14:12 +0500

    Добавлена структура данных числовой массив и линейный поиск.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace CSharpAlgorithms
+﻿using CSharpAlgorithms.Structure;
+
+namespace CSharpAlgorithms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //----------------------------структура массив чисел-----------------------------------
+            DigitsArray da = new DigitsArray(5);
+            da.Insert(1);
+            da.Insert(2);
+            da.Show();
+            da.Delete(1);
+            da.Show();
+            da.Insert(3);
+            da.Insert(4);
+            da.Show();
+            //-------------------------------------------------------------------------------------
         }
     }
 }
---
+++ Structure/DigitsArray.cs
@@ -0,0 +1,60 @@
+﻿namespace CSharpAlgorithms.Structure
+{
+    public class DigitsArray
+    {
+        private readonly int[] digits;
+        private int count;
+
+        public DigitsArray(int max)
+        {
+            digits = new int[max];
+            count = 0;
+        }
+        //---------------------------вставка числа в конец массива---------------------------------
+        public void Insert(int number)
+        {
+            digits[count++] = number;
+        }
+        //------------------------------лицейный поиск числа---------------------------------------
+        public bool Find(int searchNumber)
+        {
+            int i;
+            for (i = 0; i < count; i++)
+            {
+                if (digits[i] == searchNumber) break;
+            }
+            return (i == count) ? false : true;
+        }
+        //-----------------------удаление числа из массива с линейным поиском----------------------
+        public bool Delete(int searchNumber)
+        {
+            int i;
+            for(i = 0; i < count; i++)
+            {
+                if (digits[i] == searchNumber) break;
+            }
+            if(i == count)
+            {
+                return false;
+            }
+            else
+            {
+                for(int j = i; j < count - 1; j++)
+                {
+                    digits[j] = digits[j + 1];
+                }
+                count--;
+                return true;
+            }
+        }
+        //---------------------------вывод на экран содержимого------------------------------------
+        public void Show()
+        {
+            for(int i = 0; i < count; i++)
+            {
+                Console.Write($@"{digits[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
